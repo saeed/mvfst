ea282865f1516d34a3e68b132bea80b5fcb99263
Ahmed Saeed <saeed@alexandria.cc.gatech.edu>
2020-07-15 11:17:07 -0400

    make PTO more aggressive and acks more tight

--- quic/codec/QuicWriteCodec.cpp
+++ quic/codec/QuicWriteCodec.cpp
@@ -234,7 +234,7 @@ static size_t fillFrameWithAckBlocks(
 
     size_t additionalSize = gapSize + currBlockLenSize +
         (numAdditionalAckBlocksSize - previousNumAckBlocksSize);
-    if (bytesLimit < additionalSize + 5) {
+    if (bytesLimit < additionalSize + 20) {
 //      LOG(INFO) << "blocks exceed length" << bytesLimit << " " << additionalSize;
       break;
     }
